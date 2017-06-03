c43ae14dd5eb0d6583df7768c8303c804cc20035
gaydov <gaidov@gmail.com>
2017-06-03 14:33:56 +0300

    initial upload

---
+++ 2SetsAndDictionaries/DragonArmy/DragonArmy.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DragonArmy
+{
+    public class Dragon
+    {
+        public string Name { get; set; }
+        public double Damage { get; set; } = 45;
+        public double Health { get; set; } = 250;
+        public double Armor { get; set; } = 10;
+    }
+
+    public class DragonArmy
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<Dragon>> dragons = new Dictionary<string, List<Dragon>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string inputType = input[0];
+                string inputName = input[1];
+                string inputDamage = input[2];
+                string inputHealth = input[3];
+                string inputArmor = input[4];
+
+                Dragon currentDragon = new Dragon();
+                currentDragon.Name = inputName;
+
+                if (!inputDamage.Equals("null"))
+                {
+                    currentDragon.Damage = double.Parse(inputDamage);
+                }
+                if (!inputHealth.Equals("null"))
+                {
+                    currentDragon.Health = double.Parse(inputHealth);
+                }
+                if (!inputArmor.Equals("null"))
+                {
+                    currentDragon.Armor = double.Parse(inputArmor);
+                }
+
+                if (!dragons.ContainsKey(inputType))
+                {
+                    dragons.Add(inputType, new List<Dragon>());
+                    dragons[inputType].Add(currentDragon);
+                }
+                else
+                {
+                    if (dragons[inputType].FirstOrDefault(d => d.Name.Equals(currentDragon.Name)) == null)
+                    {
+                        dragons[inputType].Add(currentDragon);
+                    }
+                    else
+                    {
+                        dragons[inputType].FirstOrDefault(d => d.Name.Equals(currentDragon.Name)).Damage = currentDragon.Damage;
+                        dragons[inputType].FirstOrDefault(d => d.Name.Equals(currentDragon.Name)).Health = currentDragon.Health;
+                        dragons[inputType].FirstOrDefault(d => d.Name.Equals(currentDragon.Name)).Armor = currentDragon.Armor;
+                    }
+                }
+
+
+            }
+
+            PrintResults(dragons);
+        }
+
+        private static void PrintResults(Dictionary<string, List<Dragon>> dragons)
+        {
+            foreach (KeyValuePair<string, List<Dragon>> dragonType in dragons)
+            {
+                double avgDamage = dragonType.Value.Select(d => d.Damage).Average();
+                double avgHealth = dragonType.Value.Select(d => d.Health).Average();
+                double avgArmor = dragonType.Value.Select(d => d.Armor).Average();
+
+                Console.WriteLine($"{dragonType.Key}::({avgDamage:F2}/{avgHealth:F2}/{avgArmor:F2})");
+
+                foreach (Dragon dragon in dragons[dragonType.Key].OrderBy(d => d.Name))
+                {
+                    Console.WriteLine($"-{dragon.Name} -> damage: {dragon.Damage}, health: {dragon.Health}, armor: {dragon.Armor}");
+                }
+            }
+        }
+    }
+}
---
+++ 2SetsAndDictionaries/LogsAggregator/LogsAggregator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogsAggregator
+{
+    public class Session
+    {
+        public SortedSet<string> IPs { get; set; }
+        public int Duration { get; set; }
+    }
+
+    public class LogsAggregator
+    {
+        public static void Main()
+        {
+            SortedDictionary<string, Session> usersWithSessions = new SortedDictionary<string, Session>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string IP = input[0];
+                string user = input[1];
+                int duration = int.Parse(input[2]);
+
+                if (!usersWithSessions.ContainsKey(user))
+                {
+                    usersWithSessions.Add(user, new Session());
+                    usersWithSessions[user].IPs = new SortedSet<string>();
+                    usersWithSessions[user].IPs.Add(IP);
+                    usersWithSessions[user].Duration = duration;
+                }
+                else
+                {
+                    if (!usersWithSessions[user].IPs.Contains(IP))
+                    {
+                        usersWithSessions[user].IPs.Add(IP);
+                        usersWithSessions[user].Duration += duration;
+                    }
+                    else
+                    {
+                        usersWithSessions[user].Duration += duration;
+                    }
+                }
+            }
+
+            PrintResults(usersWithSessions);
+        }
+
+        private static void PrintResults(SortedDictionary<string, Session> usersWithSessions)
+        {
+            foreach (KeyValuePair<string, Session> user in usersWithSessions)
+            {
+                Console.WriteLine($"{user.Key}: {user.Value.Duration} [{string.Join(", ", user.Value.IPs)}]");
+            }
+        }
+    }
+}
