87a3052e084ea2eb6d2034fb548f334581101fe1
gaydov <gaidov@gmail.com>
2017-09-06 20:45:51 +0300

    upload bashsoft

---
+++ BashSoft/BashSoft/Repository/RepositorySorters.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BashSoft.StaticData;
+
+namespace BashSoft.Repository
+{
+    public static class RepositorySorters
+    {
+        public static void OrderAndTake(Dictionary<string, List<int>> wantedData, string comparison, int studentsToTake)
+        {
+            comparison = comparison.ToLower();
+
+            if (comparison.Equals("ascending"))
+            {
+                PrintStudents(wantedData
+                    .OrderBy(x => x.Value.Sum())
+                    .Take(studentsToTake)
+                    .ToDictionary(pair => pair.Key, pair => pair.Value));
+            }
+            else if (comparison.Equals("descending"))
+            {
+                PrintStudents(wantedData
+                    .OrderByDescending(x => x.Value.Sum())
+                    .Take(studentsToTake)
+                    .ToDictionary(pair => pair.Key, pair => pair.Value));
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.InvalidComparisonQuery);
+            }
+        }
+
+        private static void PrintStudents(Dictionary<string, List<int>> studentsSorted)
+        {
+            foreach (var student in studentsSorted)
+            {
+                OutputWriter.PrintStudent(student);
+            }
+        }
+    }
+}
---
+++ BashSoft/BashSoft/Repository/StudentsRepository.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using BashSoft.StaticData;
+
+namespace BashSoft.Repository
+{
+    public static class StudentsRepository
+    {
+        public static bool isDataInitialized = false;
+        private static Dictionary<string, Dictionary<string, List<int>>> studentsByCourse;
+
+        public static void InitializeData(string fileName)
+        {
+            if (!isDataInitialized)
+            {
+                OutputWriter.WriteMessageOnNewLine("Reading data...");
+                studentsByCourse = new Dictionary<string, Dictionary<string, List<int>>>();
+                ReadData(fileName);
+            }
+            else
+            {
+                OutputWriter.WriteMessageOnNewLine(ExceptionMessages.DataAlreadyInitializedException);
+            }
+        }
+
+        private static void ReadData(string fileName)
+        {
+            string path = SessionData.currentPath + "\\" + fileName;
+            if (File.Exists(path))
+            {
+                string pattern = @"([A-Z][a-zA-Z#+]*_[A-Z][a-z]{2}_\d{4})\s+([A-Z][a-z]{0,3}\d{2}_\d{2,4})\s+(\d+)";
+                Regex regex = new Regex(pattern);
+                string[] allInputLines = File.ReadAllLines(path);
+
+                for (int line = 0; line < allInputLines.Length; line++)
+                {
+                    if (!string.IsNullOrEmpty(allInputLines[line]) && regex.IsMatch(allInputLines[line]))
+                    {
+                        Match currentMatch = regex.Match(allInputLines[line]);
+                        string course = currentMatch.Groups[1].Value;
+                        string student = currentMatch.Groups[2].Value;
+                        int mark;
+                        bool hasParsedScore = int.TryParse(currentMatch.Groups[3].Value, out mark);
+
+                        if (hasParsedScore && 0 <= mark && mark <= 100)
+                        {
+                            if (!studentsByCourse.ContainsKey(course))
+                            {
+                                studentsByCourse.Add(course, new Dictionary<string, List<int>>());
+                            }
+
+                            if (!studentsByCourse[course].ContainsKey(student))
+                            {
+                                studentsByCourse[course].Add(student, new List<int>());
+                            }
+
+                            studentsByCourse[course][student].Add(mark);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.InvalidPath);
+                return;
+            }
+
+            isDataInitialized = true;
+            OutputWriter.WriteMessageOnNewLine("Data read!");
+        }
+
+        private static bool IsQueryForCoursePossible(string courseName)
+        {
+            if (isDataInitialized)
+            {
+                if (studentsByCourse.ContainsKey(courseName))
+                {
+                    return true;
+                }
+                else
+                {
+                    OutputWriter.DisplayException(ExceptionMessages.InexistingCourseInDataBase);
+                }
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.DataNotInitializedExceptionMessage);
+            }
+            return false;
+        }
+
+        private static bool IsQueryForStudentPossiblе(string courseName, string studentUserName)
+        {
+            if (IsQueryForCoursePossible(courseName) && studentsByCourse[courseName].ContainsKey(studentUserName))
+            {
+                return true;
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.InexistingStudentInDataBase);
+            }
+
+            return false;
+        }
+
+        public static void GetStudentScoresFromCourse(string courseName, string username)
+        {
+            if (IsQueryForStudentPossiblе(courseName, username))
+            {
+                OutputWriter.PrintStudent(new KeyValuePair<string, List<int>>(username, studentsByCourse[courseName][username]));
+            }
+        }
+
+        public static void GetAllStudentsFromCourse(string courseName)
+        {
+            if (IsQueryForCoursePossible(courseName))
+            {
+                OutputWriter.WriteMessageOnNewLine($"{courseName}:");
+                foreach (var studentsMarkEntry in studentsByCourse[courseName])
+                {
+                    OutputWriter.PrintStudent(studentsMarkEntry);
+                }
+            }
+        }
+
+        public static void FilterAndTake(string courseName, string givenFilter, int? studentsToTake = null)
+        {
+            if (IsQueryForCoursePossible(courseName))
+            {
+                if (studentsToTake == null)
+                {
+                    studentsToTake = studentsByCourse[courseName].Count;
+                }
+
+                RepositoryFilters.FilterAndTake(studentsByCourse[courseName], givenFilter, studentsToTake.Value);
+            }
+        }
+
+        public static void OrderAndTake(string courseName, string comparison, int? studentsToTake = null)
+        {
+            if (IsQueryForCoursePossible(courseName))
+            {
+                if (studentsToTake == null)
+                {
+                    studentsToTake = studentsByCourse[courseName].Count;
+                }
+
+                RepositorySorters.OrderAndTake(studentsByCourse[courseName], comparison, studentsToTake.Value);
+            }
+        }
+    }
+
+
+}
